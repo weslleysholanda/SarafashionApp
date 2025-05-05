6e7ad0467c72321ca891bd05efdc7c61a484e253
weslleysholanda <weslleyh98@gmail.com>
2025-05-04 21:26:51 -0300

    feat: adiciona telas de interface com layout inicial e estilização completa em CSS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    autoplay: {
+      delay: 3000, // tempo entre os slides (em ms)
+      disableOnInteraction: false,
+    },
+  });
